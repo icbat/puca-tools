06f072a2598eb5cf2ef1d4f2fc6cb76860ad39cf
icbat <cobb.evan@gmail.com>
2015-06-29 11:40:07 -0500

    basic error checking on run

---
+++ pucaStats.js
@@ -0,0 +1,17 @@
+var pucastats {
+
+    // Responsible for initial loading and setup
+    setup: function () {
+        if ( window.location.href.toLowerCase().indexOf('pucatrade.com') === -1 ) {
+            alert('Hey!  This doesn\'t look like PucaTrade!');
+            return this;
+        }
+
+        if ( window.location.href.toLowerCase().indexOf('pucatrade.com/history') === -1 ) {
+            alert('Hey!  This isn\'t the History section!');
+            return this;
+        }
+
+        return this;
+    }
+}.setup();
