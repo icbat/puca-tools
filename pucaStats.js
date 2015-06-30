b8eb2d94f8cba2d51f2dd00ef522609f16c5c014
icbat <cobb.evan@gmail.com>
2015-06-30 12:37:31 -0500

    adding the correct history path
    I'm smart.

--- pucaStats.js
+++ pucaStats.js
@@ -7,7 +7,7 @@ var pucastats = {
             return this;
         }
 
-        if ( window.location.href.toLowerCase().indexOf('pucatrade.com/history') === -1 ) {
+        if ( window.location.href.toLowerCase().indexOf('pucatrade.com/trades/past/sent') === -1 ) {
             alert('Hey!  This isn\'t the History section!');
             return this;
         }
